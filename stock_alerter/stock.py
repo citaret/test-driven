2a001fa09d22706676e45f9fcc3b0b0d53ed0690
citaret@iMac <citaret@gmail.com>
2016-10-27 16:53:50 +0800

    code smells

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,4 +1,7 @@
 import bisect
+import collections
+
+PriceEvent = collections.namedtuple("PriceEvent", ["timestamp", "price"])
 
 class Stock:
     def __init__(self, symbol):
@@ -9,11 +12,46 @@ class Stock:
         if price < 0:
             raise ValueError("price should not be negative")
         # self.price_history.append(price)
-        bisect.insort(self.price_history, (timestamp, price))
+        bisect.insort(self.price_history, PriceEvent(timestamp, price))
 
     @property
     def price(self):
-        return self.price_history[-1][1] if self.price_history else None
+        return self.price_history[-1].price if self.price_history else None
 
     def is_increasing_trend(self):
-        return self.price_history[-3][1] < self.price_history[-2][1] < self.price_history[-1][1]
+        return self.price_history[-3].price < self.price_history[-2].price < self.price_history[-1].price
+
+    def get_crossover_signal(self, on_date):
+        cpl = []
+        for i in range(11):
+            chk = on_date.date() - timedelta(i)
+            for price_event in reversed(self.price_history):
+                if price_event.timestamp.date() > chk:
+                    pass
+                if price_event.timestamp.date() == chk:
+                    cpl.insert(0, price_event)
+                    break
+                if price_event.timestamp.date() < chk:
+                    cpl.insert(0, price_event)
+                    break
+
+        # Return NEUTRAL signal
+        if len(cpl) < 11:
+            return 0
+
+        # BUY signal
+        if sum([update.price for update in cpl[-11:-1]])/10 \
+                > sum([update.price for update in cpl[-6:-1]])/5 \
+            and sum([update.price for update in cpl[-10:]])/10 \
+                < sum([update.price for update in cpl[-5:]])/5:
+                    return 1
+
+        # BUY signal
+        if sum([update.price for update in cpl[-11:-1]])/10 \
+                < sum([update.price for update in cpl[-6:-1]])/5 \
+            and sum([update.price for update in cpl[-10:]])/10 \
+                > sum([update.price for update in cpl[-5:]])/5:
+                    return -1
+
+        # NEUTRAL signal
+        return 0
