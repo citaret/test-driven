0538f0942b04e4d9e65a983d47f5107e7a3d166d
citaret@iMac <citaret@gmail.com>
2016-10-27 15:04:04 +0800

    exercise

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,9 +1,19 @@
+import bisect
+
 class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
-        self.price = None
+        self.price_history = []
 
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("price should not be negative")
-        self.price = price
+        # self.price_history.append(price)
+        bisect.insort(self.price_history, (timestamp, price))
+
+    @property
+    def price(self):
+        return self.price_history[-1][1] if self.price_history else None
+
+    def is_increasing_trend(self):
+        return self.price_history[-3][1] < self.price_history[-2][1] < self.price_history[-1][1]
