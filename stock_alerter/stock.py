b1f460d884656d296aeadbb2edee65a9105ed8ac
citaret@iMac <citaret@gmail.com>
2016-10-28 11:07:49 +0800

    moving method

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,11 +1,6 @@
-import bisect
-import collections
 from enum import Enum
-from datetime import timedelta
 
 from timeseries import TimeSeries
-
-PriceEvent = collections.namedtuple("PriceEvent", ["timestamp", "price"])
 
 class StockSignal(Enum):
     buy = 1
@@ -18,13 +13,11 @@ class Stock:
 
     def __init__(self, symbol):
         self.symbol = symbol
-        self.price_history = []
         self.history = TimeSeries()
 
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("price should not be negative")
-        bisect.insort(self.price_history, PriceEvent(timestamp, price))
         self.history.update(timestamp, price)
 
     @property
@@ -37,29 +30,13 @@ class Stock:
     def is_increasing_trend(self):
         return self.history[-3].value < self.history[-2].value < self.history[-1].value
 
-    def _get_closing_price_list(self, on_date, num_days):
-        closing_price_list = []
-
-        for i in range(num_days):
-            chk = on_date.date() - timedelta(i)
-            for price_event in reversed(self.price_history):
-                if price_event.timestamp.date() > chk:
-                    pass
-                if price_event.timestamp.date() == chk:
-                    closing_price_list.insert(0, price_event)
-                    break
-                if price_event.timestamp.date() < chk:
-                    closing_price_list.insert(0, price_event)
-                    break
-        return closing_price_list
-
     def _is_crossover_below_to_above(self, prev_ma, prev_reference_ma,
                                          current_ma, current_reference_ma):
         return prev_ma < prev_reference_ma and current_ma > current_reference_ma
 
     def get_crossover_signal(self, on_date):
         NUM_DAYS = self.LONG_TERM_TIMESPAN + 1
-        closing_price_list = self._get_closing_price_list(on_date, NUM_DAYS)
+        closing_price_list = self.history.get_closing_price_list(on_date, NUM_DAYS)
 
         if len(closing_price_list) < NUM_DAYS:
             return StockSignal.neutral
@@ -71,13 +48,13 @@ class Stock:
         prev_short_term_series = \
                 closing_price_list[-self.SHORT_TERM_TIMESPAN-1:-1]
 
-        long_term_ma = sum([update.price
+        long_term_ma = sum([update.value
             for update in long_term_series]) / self.LONG_TERM_TIMESPAN
-        prev_long_term_ma = sum([update.price
+        prev_long_term_ma = sum([update.value
             for update in prev_long_term_series]) / self.LONG_TERM_TIMESPAN
-        short_term_ma = sum([update.price
+        short_term_ma = sum([update.value
             for update in short_term_series]) / self.SHORT_TERM_TIMESPAN
-        prev_short_term_ma = sum([update.price
+        prev_short_term_ma = sum([update.value
             for update in prev_short_term_series]) / self.SHORT_TERM_TIMESPAN
 
         if self._is_crossover_below_to_above(prev_short_term_ma, prev_long_term_ma,
@@ -89,7 +66,3 @@ class Stock:
                 return StockSignal.sell
 
         return StockSignal.neutral
-
-
-if __name__ == "__main__":
-    pass
