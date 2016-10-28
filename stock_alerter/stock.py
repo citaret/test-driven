21f878f1ab2ffb17e7b73347c7d639ed6daa9eb2
citaret@iMac <citaret@gmail.com>
2016-10-28 17:53:30 +0800

    ch3 exercise

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,11 +1,14 @@
+from datetime import timedelta
 from enum import Enum
 
-from timeseries import TimeSeries
+from .timeseries import TimeSeries, MovingAverage, NotEnoughDataException
+
 
 class StockSignal(Enum):
     buy = 1
     neutral = 0
     sell = -1
+
 
 class Stock:
     LONG_TERM_TIMESPAN = 10
@@ -15,11 +18,6 @@ class Stock:
         self.symbol = symbol
         self.history = TimeSeries()
 
-    def update(self, timestamp, price):
-        if price < 0:
-            raise ValueError("price should not be negative")
-        self.history.update(timestamp, price)
-
     @property
     def price(self):
         try:
@@ -27,42 +25,29 @@ class Stock:
         except IndexError:
             return None
 
+    def update(self, timestamp, price):
+        if price < 0:
+            raise ValueError("price should not be negative")
+        self.history.update(timestamp, price)
+
     def is_increasing_trend(self):
         return self.history[-3].value < self.history[-2].value < self.history[-1].value
 
-    def _is_crossover_below_to_above(self, prev_ma, prev_reference_ma,
-                                         current_ma, current_reference_ma):
-        return prev_ma < prev_reference_ma and current_ma > current_reference_ma
+    def _is_crossover_below_to_above(self, on_date, ma, reference_ma):
+        prev_date = on_date - timedelta(1)
+        return (ma.value_on(prev_date) < reference_ma.value_on(prev_date)
+                and ma.value_on(on_date) > reference_ma.value_on(on_date))
 
     def get_crossover_signal(self, on_date):
-        NUM_DAYS = self.LONG_TERM_TIMESPAN + 1
-        closing_price_list = self.history.get_closing_price_list(on_date, NUM_DAYS)
-
-        if len(closing_price_list) < NUM_DAYS:
+        long_term_ma = MovingAverage(self.history, self.LONG_TERM_TIMESPAN)
+        short_term_ma = MovingAverage(self.history, self.SHORT_TERM_TIMESPAN)
+        try:
+            if self._is_crossover_below_to_above( on_date, short_term_ma, long_term_ma):
+                return StockSignal.buy
+            if self._is_crossover_below_to_above( on_date, long_term_ma, short_term_ma):
+                return StockSignal.sell
+        except NotEnoughDataException:
             return StockSignal.neutral
 
-        long_term_series = closing_price_list[-self.LONG_TERM_TIMESPAN:]
-        prev_long_term_series = \
-                closing_price_list[-self.LONG_TERM_TIMESPAN-1:-1]
-        short_term_series = closing_price_list[-self.SHORT_TERM_TIMESPAN:]
-        prev_short_term_series = \
-                closing_price_list[-self.SHORT_TERM_TIMESPAN-1:-1]
-
-        long_term_ma = sum([update.value
-            for update in long_term_series]) / self.LONG_TERM_TIMESPAN
-        prev_long_term_ma = sum([update.value
-            for update in prev_long_term_series]) / self.LONG_TERM_TIMESPAN
-        short_term_ma = sum([update.value
-            for update in short_term_series]) / self.SHORT_TERM_TIMESPAN
-        prev_short_term_ma = sum([update.value
-            for update in prev_short_term_series]) / self.SHORT_TERM_TIMESPAN
-
-        if self._is_crossover_below_to_above(prev_short_term_ma, prev_long_term_ma,
-                                             short_term_ma, long_term_ma):
-                return StockSignal.buy
-
-        if self._is_crossover_below_to_above(prev_long_term_ma, prev_short_term_ma,
-                                             long_term_ma, short_term_ma):
-                return StockSignal.sell
-
         return StockSignal.neutral
+
--- stock_alerter/timeseries.py
+++ stock_alerter/timeseries.py
@@ -1,6 +1,6 @@
 import bisect
 import collections
-from datetime import timedelta
+import datetime
 
 Update = collections.namedtuple("Update", ["timestamp", "value"])
 
@@ -17,7 +17,7 @@ class TimeSeries:
     def get_closing_price_list(self, on_date, num_days):
         closing_price_list = []
         for i in range(num_days):
-            chk = on_date.date() - timedelta(i)
+            chk = on_date.date() - datetime.timedelta(i)
             for price_event in reversed(self.series):
                 if price_event.timestamp.date() > chk:
                     pass
@@ -28,4 +28,20 @@ class TimeSeries:
                     closing_price_list.insert(0, price_event)
                     break
         return closing_price_list
+
+class MovingAverage:
+    def __init__(self, series, timespan):
+        self.series = series
+        self.timespan = timespan
+
+    def value_on(self, end_date):
+        moving_average_range = self.series.get_closing_price_list(
+                                   end_date, self.timespan)
+        if len(moving_average_range) < self.timespan:
+            raise NotEnoughDataException("Not enough data")
+        price_list = [item.value for item in moving_average_range]
+        return sum(price_list)/len(price_list)
+
+class NotEnoughDataException(Exception):
+    pass
 
