22c7a46c499d0565da58901f35e2d44d27d3db3f
citaret@iMac <citaret@gmail.com>
2016-11-04 18:30:16 +0800

    inject dependencies

--- stock_alerter/legacy.py
+++ stock_alerter/legacy.py
@@ -4,8 +4,11 @@ from .stock import Stock
 from .rule import PriceRule
 
 class AlertProcessor:
-    def __init__(self):
-        self.exchange = {"GOOG": Stock("GOOG"), "AAPL": Stock("AAPL")}
+    def __init__(self, autorun=True, exchange=None):
+        if exchange is None:
+            self.exchange = {"GOOG": Stock("GOOG"), "AAPL": Stock("AAPL")}
+        else:
+            self.exchange = exchange
         rule_1 = PriceRule("GOOG", lambda stock: stock.price > 10)
         rule_2 = PriceRule("AAPL", lambda stock: stock.price > 5)
         self.exchange["GOOG"].updated.connect(
@@ -14,7 +17,8 @@ class AlertProcessor:
         self.exchange["AAPL"].updated.connect(
             lambda stock: print(stock.symbol, stock.price) \
                 if rule_2.matches(self.exchange) else None)
-        self.run()
+        if autorun:
+            self.run()
 
     def run(self):
         updates = []
@@ -22,7 +26,9 @@ class AlertProcessor:
             for line in fp.readlines():
                 symbol, timestamp, price = line.split(",")
                 updates.append((symbol, datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f"), int(price)))
+        self.do_updates(updates)
 
+    def do_updates(self, updates):
         for symbol, timestamp, price in updates:
             stock = self.exchange[symbol]
             stock.update(timestamp, price)
--- stock_alerter/tests/test_legacy.py
+++ stock_alerter/tests/test_legacy.py
@@ -4,6 +4,12 @@ from datetime import datetime
 
 from ..legacy import AlertProcessor
 
+# class TestAlertProcessor(AlertProcessor):
+#     def __init__(self, exchange):
+#         AlertProcessor.__init__(self, autorun=False)
+#         self.exchange = exchange
+
+
 class AlertProcessorTest(unittest.TestCase):
     @mock.patch("builtins.print")
     def test_processor_characterization_1(self, mock_print):
@@ -12,21 +18,6 @@ class AlertProcessorTest(unittest.TestCase):
                                      mock.call("GOOG", 15),
                                      mock.call("AAPL", 10),
                                      mock.call("GOOG", 21)])
-
-# class TestAlertProcessor(AlertProcessor):
-#     def __init__(self, exchange):
-#         AlertProcessor.__init__(self, autorun=False)
-#         self.exchange = exchange
-
-
-# class AlertProcessorTest(unittest.TestCase):
-#     @mock.patch("builtins.print")
-#     def test_processor_characterization_1(self, mock_print):
-#         AlertProcessor()
-#         mock_print.assert_has_calls([mock.call("AAPL", 8),
-#                                      mock.call("GOOG", 15),
-#                                      mock.call("AAPL", 10),
-#                                      mock.call("GOOG", 21)])
 
 #     def test_processor_characterization_2(self):
 #         processor = AlertProcessor(autorun=False)
@@ -37,22 +28,22 @@ class AlertProcessorTest(unittest.TestCase):
 #                                      mock.call("AAPL", 10),
 #                                      mock.call("GOOG", 21)])
 
-#     def test_processor_characterization_3(self):
-#         processor = AlertProcessor(autorun=False)
-#         mock_goog = mock.Mock()
-#         processor.exchange = {"GOOG": mock_goog}
-#         updates = [("GOOG", datetime(2014, 12, 8), 5)]
-#         processor.do_updates(updates)
-#         mock_goog.update.assert_called_with(datetime(2014, 12, 8), 5)
+    def test_processor_characterization_3(self):
+        processor = AlertProcessor(autorun=False)
+        mock_goog = mock.Mock()
+        processor.exchange = {"GOOG": mock_goog}
+        updates = [("GOOG", datetime(2014, 12, 8), 5)]
+        processor.do_updates(updates)
+        mock_goog.update.assert_called_with(datetime(2014, 12, 8), 5)
 
-#     def test_processor_characterization_4(self):
-#         mock_goog = mock.Mock()
-#         mock_aapl = mock.Mock()
-#         exchange = {"GOOG": mock_goog, "AAPL": mock_aapl}
-#         processor = AlertProcessor(autorun=False, exchange=exchange)
-#         updates = [("GOOG", datetime(2014, 12, 8), 5)]
-#         processor.do_updates(updates)
-#         mock_goog.update.assert_called_with(datetime(2014, 12, 8), 5)
+    def test_processor_characterization_4(self):
+        mock_goog = mock.Mock()
+        mock_aapl = mock.Mock()
+        exchange = {"GOOG": mock_goog, "AAPL": mock_aapl}
+        processor = AlertProcessor(autorun=False, exchange=exchange)
+        updates = [("GOOG", datetime(2014, 12, 8), 5)]
+        processor.do_updates(updates)
+        mock_goog.update.assert_called_with(datetime(2014, 12, 8), 5)
 
 #     def test_processor_characterization_5(self):
 #         mock_goog = mock.Mock()
